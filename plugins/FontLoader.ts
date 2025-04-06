d53cc9a7d2fff99c52e1f0dd9293dcc100b7f687
mariana-reis <mariana.reis@al.resilia.com.br>
2025-04-06 18:57:41 -0300

    :lipstick: style: configure custom Google Fonts with FontLoader plugin

---
+++ plugins/FontLoader.ts
@@ -0,0 +1,19 @@
+export default defineNuxtPlugin(() => {
+  useHead({
+    link: [
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.googleapis.com',
+      },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+        crossorigin: 'anonymous',
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Playfair:ital,opsz,wght@0,5..1200,300..900;1,5..1200,300..900&display=swap',
+      },
+    ],
+  })
+})
